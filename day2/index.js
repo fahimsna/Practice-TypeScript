d5d963a49da25c3279d0e7a28e3fe123d2130c32
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-16 10:12:00 +0600

    added dataTypes of typeScript

--- day2/index.js
+++ day2/index.js
@@ -10,4 +10,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // console.log(userName);
 // userName = "Fahim";
 // console.log(userName);
+// let user: null | string = null;
+// console.log(user);
+let l = [10, 20, 30, 40];
+console.log(l);
+l.push(99);
+console.log(l);
+let userList = ["Fahim", "Shahriar"];
+console.log(userList);
+let m = [10, 20, "fahim"];
+console.log(m);
+let demo = [10, "fahim"];
+console.log(demo);
+let color = ["red", "blue"];
+console.log(color);
+let userObj = {
+    name: "fahim",
+    age: 25,
+};
+console.log(userObj);
+let userAddress = {
+    name: "Fahim",
+    email: "fahim@test.com",
+    address: {
+        pinCode: 1200,
+        city: "Dhaka",
+    },
+};
+console.log(userAddress);
 //# sourceMappingURL=index.js.map
--- day2/index.ts
+++ day2/index.ts
@@ -13,3 +13,49 @@
 // userName = "Fahim";
 // console.log(userName);
 
+// let user: null | string = null;
+// console.log(user);
+
+let l: Number[] = [10, 20, 30, 40];
+console.log(l);
+l.push(99);
+console.log(l);
+
+let userList: string[] = ["Fahim", "Shahriar"];
+console.log(userList);
+
+let m: any[] = [10, 20, "fahim"];
+console.log(m);
+
+let demo: [Number, string] = [10, "fahim"];
+console.log(demo);
+let color: readonly String[] = ["red", "blue"];
+console.log(color);
+
+let userObj: { name: string; age: Number } = {
+  name: "fahim",
+  age: 25,
+};
+console.log(userObj);
+
+let userAddress: {
+  name: string;
+  email: string;
+  address: {
+    pinCode: number;
+    city: string;
+  };
+} = {
+  name: "Fahim",
+  email: "fahim@test.com",
+  address: {
+    pinCode: 1200,
+    city: "Dhaka",
+  },
+};
+console.log(userAddress);
+
+type userobj = {
+  name: string;
+  age: number;
+};
