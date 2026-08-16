107742acd4e9e9ea1199bef01338c180f3804517
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-16 10:37:38 +0600

    Function

---
+++ day3/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addData(num1, num2) {
+    return num1 + num2;
+}
+console.log(addData(2, 3));
+//# sourceMappingURL=index.js.map
